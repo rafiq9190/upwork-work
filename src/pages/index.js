35ca2ffafc5d9d5afd49641e9d4090a9ffeba1a3
Muhammad Rafiq <muhammadrafiq017@gmail.com>
2022-07-26 07:28:55 +0500

    css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-// import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 import Login from "../components/Login"
 
